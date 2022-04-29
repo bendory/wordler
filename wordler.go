1c1745a0ab7c4c824e2503a16192022e716be433
David Bendory <bendory@users.noreply.github.com>
2022-04-29 09:23:53 -0400

    Comments.

--- wordler.go
+++ wordler.go
@@ -1,7 +1,12 @@
 package wordler
 
 const (
-	CORRECT   = '+'
+	// CORRECT indicates that the letter is in the puzzle in the given location.
+	CORRECT = '+'
+
+	// ELSEWHERE indicates that the letter is somewhere else in the puzzle.
 	ELSEWHERE = '*'
-	NIL       = '_'
+
+	// NIL indicates that the letter is not found in the puzzle.
+	NIL = '_'
 )
