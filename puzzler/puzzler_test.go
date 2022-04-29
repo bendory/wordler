2e1687244aa5eecd33a7d323b9d1c2272311e9c2
David Bendory <bendory@users.noreply.github.com>
2022-04-29 09:23:53 -0400

    Initial revision.:

---
+++ puzzler/puzzler_test.go
@@ -0,0 +1,35 @@
+package wordler
+
+import (
+	"testing"
+
+	"wordler/wordlist"
+)
+
+type fakeLoader struct {
+	words []string // words to load
+	err   error    // optional error for testing error paths
+}
+
+func (f fakeLoader) Load(options ...wordlist.Option) (*wordlist.WordList, error) {
+	return wordlist.New(f.words, options...), f.err
+}
+
+func TestNew(t *testing.T) {
+	list := []string{"foo", "bar", "bam", "zap"}
+	wordlist.Loader = fakeLoader{words: list}
+
+	p, err := New(true)
+	if err != nil {
+		t.Errorf("error: %v", err)
+	}
+	if want, got := len(list), p.dict.Length(); want != got {
+		t.Errorf("want %#v; got %#v", list, p.dict)
+	}
+	if !p.dict.Equals(p.remaining) {
+		t.Errorf("%#v != %#v", p.dict, p.remaining)
+	}
+	if !p.dict.Contains(p.word) {
+		t.Errorf("dict is missing %v", p.word)
+	}
+}
