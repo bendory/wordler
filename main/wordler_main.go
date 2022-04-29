f5bb9516c6d7e064dd6a2d85d13eaad2cb11d092
David Bendory <bendory@users.noreply.github.com>
2022-04-29 09:23:56 -0400

    Cleanup, debug output, whitespace.

--- main/wordler_main.go
+++ main/wordler_main.go
@@ -61,25 +61,32 @@ func main() {
 		debug(-1, "Iteration %d/%d: ", i+1, *iterations)
 		p, err := puzzler.New(args)
 		if err != nil {
+			// This should never happen.
+			count.puzzlerFailures++
 			fmt.Printf("Failed to make a Puzzler: %v\n", err)
 			continue
 		}
 		s, err := solver.New(option)
 		if err != nil {
+			// This should never happen.
+			count.solverFailures++
 			fmt.Printf("Failed to make a Solver: %v\n", err)
 			continue
 		}
 
 		var guess, response string
 		var guesses []string
+	OUTER: // Loop until we win, get an error, or run out of guesses.
 		for p.Guesses() > 0 {
 			if p.Words() != s.Remaining() {
 				fmt.Printf("  ERROR: %d Puzzler words != %d Solver words (continuing anyway)\n", p.Words(), s.Remaining())
 			}
 			debug(0, "  %d guesses and %d words remain.", p.Guesses(), p.Words())
 
-		GUESS:
+		GUESS: // Loop until we get a valid guess.
 			for {
+				// Exhaust guesses specified on the command line, then use our
+				// guessing algorithm.
 				if len(clGuesses) > 0 {
 					guess = clGuesses[0]
 					clGuesses = clGuesses[1:]
@@ -89,34 +96,45 @@ func main() {
 				guesses = append(guesses, guess)
 				response, err = p.Guess(guess)
 				switch err {
+
+				// This should never happen given that puzzler and solver use
+				// the same dictionary.
 				case puzzler.InvalidGuessErr, puzzler.NotInDictionaryErr:
 					fmt.Printf("  Invalid guess '%v': %v\n", guess, err)
 					count.invalidGuesses++
 					s.NotInWordle(guess)
+
+				// This should never happen; we should break out of OUTER before
+				// getting this error.
 				case puzzler.OutOfGuessesErr:
 					count.outOfGuesses++
-					break GUESS
+					break OUTER
+
+				// This should never happen; we should either run out of guesses
+				// or win first.
 				case puzzler.NoWordsRemainingErr:
 					fmt.Println("  Uh oh, no words remaining in Puzzler!?")
 					count.noWordsRemaining++
-					break GUESS
+					break OUTER
+
+				// Expected behavior -- valid guess.
 				case nil:
 					break GUESS
 				}
 			}
+
 			if response == winningResponse {
-				debug(-1, "  WINNER! '%v' is the word!", guess)
 				break
-			} else {
-				debug(1, "  '%v' --> '%v'", guess, response)
-				if err = s.React(guess, response); err != nil {
-					count.badReactions++
-					fmt.Printf("  ERROR: guess '%v' --> %v\n", guess, err)
-				}
+			}
+			debug(1, "  '%v' --> '%v'", guess, response)
+			if err = s.React(guess, response); err != nil {
+				count.badReactions++
+				fmt.Printf("  ERROR: guess '%v' --> %v\n", guess, err)
 			}
 		}
 
 		if response == winningResponse {
+			debug(-1, "  WINNER! '%v' is the word! Guesses: %v", guess, strings.Join(guesses, ", "))
 			count.winners++
 			count.winningIteration += float32(args.Guesses - p.Guesses())
 		} else if p.Guesses() == 0 {
