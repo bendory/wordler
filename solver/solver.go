fa42ee73f4ac5b7c63455c12dc368cdeff5cd756
David Bendory <bendory@users.noreply.github.com>
2022-05-01 12:41:33 -0400

    Fixed comment.

--- solver/solver.go
+++ solver/solver.go
@@ -43,7 +43,7 @@ func New(options ...wordlist.Option) (*Solver, error) {
 	return g, err
 }
 
-// Guess provides a random guess from remaining words
+// Guess provides a guess from remaining words
 func (s *Solver) Guess() string {
 	return s.w.OptimalGuess()
 }
